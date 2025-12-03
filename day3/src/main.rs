192c30052ad85ec2d4237a94f34a2cb3841984dc
Jonathon Jongsma <jjongsma@redhat.com>
2025-12-03 10:49:43 -0600

    day3: keep battery bank cells as u8 for processing
    Signed-off-by: Jonathon Jongsma <jjongsma@redhat.com>

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,18 +1,20 @@
-use std::time::SystemTime;
+use std::time::{Duration, SystemTime};
 
-use anyhow::anyhow;
 use tracing::{debug, instrument, warn};
+
+fn time<T, F: Fn() -> T>(f: F) -> (T, Duration) {
+    let start = SystemTime::now();
+    (f(), start.elapsed().expect("failed to get elapsed time"))
+}
 
 fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
     let input = std::fs::read_to_string("input")?;
-    let banks = parse(&input)?;
-    let start = SystemTime::now();
-    let sum = part1(&banks);
-    println!("Part 1: {sum} (time: {:?})", start.elapsed()?);
-    let start = SystemTime::now();
-    let sum = part2(&banks);
-    println!("Part 2: {sum} (time: {:?})", start.elapsed()?);
+    let banks = parse(&input);
+    let (sum, elapsed) = time(|| part1(&banks));
+    println!("Part 1: {sum} (time: {elapsed:?})");
+    let (sum, elapsed) = time(|| part2(&banks));
+    println!("Part 2: {sum} (time: {elapsed:?})");
     Ok(())
 }
 
@@ -26,7 +28,7 @@ fn part2(banks: &[BatteryBank]) -> u64 {
 
 #[derive(Debug)]
 struct BatteryBank {
-    cells: Vec<u32>,
+    cells: Vec<u8>,
 }
 
 impl BatteryBank {
@@ -40,7 +42,7 @@ impl BatteryBank {
             first_elements
                 .iter()
                 .enumerate()
-                .fold((0, 0), |acc: (u32, usize), (i, item)| {
+                .fold((0, 0), |acc: (u8, usize), (i, item)| {
                     let mut max = acc.0;
                     let mut position = acc.1;
                     if *item > max {
@@ -56,7 +58,7 @@ impl BatteryBank {
             .iter()
             .max()
             .expect("Unable to find max of remaining digits");
-        first_digit * 10 + second_digit
+        first_digit as u32 * 10 + *second_digit as u32
     }
 
     fn max_joltage_2(&self) -> u64 {
@@ -73,43 +75,35 @@ impl BatteryBank {
 }
 
 #[instrument(ret, level = "debug")]
-fn find_max_ignoring_end_n(cells: &[u32], end_n: usize) -> (u32, usize) {
+fn find_max_ignoring_end_n(cells: &[u8], end_n: usize) -> (u8, usize) {
     let (first_elements, _last_elements) = cells.split_at(cells.len() - end_n);
     debug!(?first_elements);
-    let (first_digit, positions) = first_elements.iter().enumerate().fold(
-        (0, Vec::default()),
-        |acc: (u32, Vec<usize>), (i, item)| {
-            debug!(?acc, i, item);
-            let mut max = acc.0;
-            let mut positions = acc.1;
-            if *item > max {
-                max = *item;
-                positions.clear();
-                positions.push(i);
-            } else if *item == max {
-                positions.push(i);
-            }
-            (max, positions)
-        },
-    );
-    debug!(?first_digit, ?positions);
-    let pos = positions.first().expect("positions was empty");
-    (first_digit, *pos)
+    let (first_digit, position) =
+        first_elements
+            .iter()
+            .enumerate()
+            .fold((0, 0), |acc: (u8, usize), (i, item)| {
+                debug!(?acc, i, item);
+                let mut max = acc.0;
+                let mut position = acc.1;
+                if *item > max {
+                    max = *item;
+                    position = i;
+                }
+                (max, position)
+            });
+    debug!(?first_digit, ?position);
+    let pos = position;
+    (first_digit, pos)
 }
 
-fn parse(input: &str) -> anyhow::Result<Vec<BatteryBank>> {
+fn parse(input: &str) -> Vec<BatteryBank> {
     input
         .lines()
-        .map(|line| {
-            line.chars()
-                .map(|ch| {
-                    ch.to_digit(10)
-                        .ok_or_else(|| anyhow!("Failed to convert {ch} to a digit"))
-                })
-                .collect::<Result<_, _>>()
-                .map(|v| BatteryBank { cells: v })
+        .map(|line| BatteryBank {
+            cells: line.chars().map(|ch| ch as u8).collect::<Vec<_>>(),
         })
-        .collect::<Result<Vec<_>, _>>()
+        .collect::<Vec<_>>()
 }
 
 #[cfg(test)]
@@ -124,7 +118,7 @@ mod test {
 
     #[test]
     fn test_part1() {
-        let batteries = parse(TEST_INPUT).expect("Failed to parse input");
+        let batteries = parse(TEST_INPUT);
         assert_eq!(98, batteries[0].max_joltage());
         assert_eq!(89, batteries[1].max_joltage());
         assert_eq!(78, batteries[2].max_joltage());
@@ -133,7 +127,7 @@ mod test {
 
     #[test]
     fn test_part2() {
-        let batteries = parse(TEST_INPUT).expect("Failed to parse input");
+        let batteries = parse(TEST_INPUT);
         assert_eq!(987654321111, batteries[0].max_joltage_2());
         assert_eq!(811111111119, batteries[1].max_joltage_2());
         assert_eq!(434234234278, batteries[2].max_joltage_2());
@@ -142,7 +136,7 @@ mod test {
 
     #[test]
     fn test_parse() {
-        let batteries = parse(TEST_INPUT).expect("Failed to parse input");
+        let batteries = parse(TEST_INPUT);
         assert_eq!(4, batteries.len());
         assert_eq!(9, batteries[0].cells[0]);
         assert_eq!(2, batteries[0].cells[7]);
